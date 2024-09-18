c609aa2704ad7d5c6adea2a0f5dec9224a447a2a
zhengqw <3182353772@qq.com>
2024-09-18 16:32:30 +0800

    再次提交

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    页面显示是什么
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  // },
+
 }
 </script>
 
---
+++ src/view/test01.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- <el-button @click="show3 = !show3">Click Me</el-button>
+
+        <div style="margin-top: 20px; height: 200px;">
+            <el-collapse-transition>
+                <div v-show="show3">
+                    <div class="transition-box">el-collapse-transition</div>
+                    <div class="transition-box">el-collapse-transition</div>
+                </div>
+            </el-collapse-transition>
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
