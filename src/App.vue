902b04600db175133995eff851d126d6df8ac1fe
zhengqw <3182353772@qq.com>
2024-09-24 20:29:52 +0800

    css边框流动效果

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    页面显示是什么
+    <div class="box">
+      <h2>css</h2>
+    </div>
   </div>
 </template>
 
@@ -11,6 +13,8 @@ export default {
   name: 'App',
   // components: {
   // },
+  methods: {
+  }
 
 }
 </script>
@@ -18,10 +22,55 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-left: 350px;
+  padding-top: 350px;
+}
+
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 180px;
+  height: 250px;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 20px;
+  overflow: hidden;
+
+  h2 {
+    color: white;
+    font-size: 4em;
+    text-shadow: 2px 2px pink;
+    z-index: 1;
+  }
+}
+
+.box::before {
+  content: '';
+  position: absolute;
+  width: 120px;
+  height: 120%;
+  background: linear-gradient(#00ccff, #d500f9);
+  animation: rotate 4s linear infinite;
+}
+
+.box::after {
+  content: '';
+  position: absolute;
+  background: #0e1538;
+  inset: 5px;
+  /* top:5px;left:5px;bottom:5px;right:5px */
+  border-radius: 20px;
+}
+
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
